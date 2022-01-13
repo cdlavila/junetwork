2775753c7d5b23bdf127be7b3b2a041b2edfcd5e
Carlos <carlosdaniel.londono@utp.edu.co>
2022-01-13 13:13:37 -0500

    Edit user seeder

--- api/database/seeders/20211030231313-sample-users.js
+++ api/database/seeders/20211030231313-sample-users.js
@@ -5,29 +5,32 @@ module.exports = {
     await queryInterface.bulkInsert('users', [{
       id: '8d4169b9-673b-4819-8097-376548c51a15',
       name: 'Carlos Daniel Londoño Avila',
+      picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZO_WVXbxRGfF30LW1tFdTPGnrjSjPc4fWYN8YkLMAH_wvYxoAg_T6ky9wfkEXtDH-wM4&usqp=CAU',
       birthday: new Date('2001-08-10'), // YYYY-MM-DD
       gender: 'male',
       phone: '3205821741',
       email: 'carlosdaniellondonoavila@gmail.com',
-      password: '$2b$10$idBDtYf.D3CFeHV1Spm.COrQ9gagnRLT081TLw6eGHTcKSjc1Z7Ba',
+      password: 'carlos123',
       creation_date: new Date()
     }, {
       id: 'fce08757-00e4-4566-9042-1c3de54ff5b0',
       name: 'Lina Maria Londoño Avila',
+      picture: 'https://static.wikia.nocookie.net/disney/images/1/13/Gal_Gadot.jpg/revision/latest?cb=20190105181535&path-prefix=es',
       birthday: new Date('1980-03-07'), // YYYY-MM-DD
       gender: 'female',
       phone: '3126233178',
       email: 'linamaria@gmail.com',
-      password: '$2b$10$Sa8GfclDuwPDGxHXXEwOVOjcdq7GiBQb/CHFRkP1TrlYRan9J0alm',
+      password: 'lina123',
       creation_date: new Date()
     }, {
       id: '82afd153-cc39-42c7-9481-69f5162c3848',
       name: 'Daniela Velarde Arana',
+      picture: 'https://static.wikia.nocookie.net/doblaje/images/9/9b/Scarlet_Johansson_2019.jpg/revision/latest/top-crop/width/360/height/450?cb=20190423235236&path-prefix=es',
       birthday: new Date('2000-07-04'), // YYYY-MM-DD
       gender: 'female',
       phone: '3128236809',
       email: 'velarded44@gmail.com',
-      password: '$2b$10$R.rI/xj0sdbSmJsk5oTeqOTm6dNFeYU973mFFKgjLjWNZqfq.YCJ6',
+      password: 'daniela123',
       creation_date: new Date()
     }], {})
   },
