db17c923c1a5f98f1e8a48fff8b074ef12fc2a49
Carlos <carlosdaniel.londono@utp.edu.co>
2022-01-25 01:00:51 -0500

    Rename env variable DB_URL to DATABASE_URL

--- api/database/config.js
+++ api/database/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    url: process.env.DB_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     define: {
@@ -11,7 +11,7 @@ module.exports = {
     }
   },
   test: {
-    url: process.env.DB_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     define: {
@@ -20,7 +20,7 @@ module.exports = {
     }
   },
   production: {
-    url: process.env.DB_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
