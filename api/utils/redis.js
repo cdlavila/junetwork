26b28f00b60d542b2b5733b6ae50532d8c3bd8fa
Carlos <carlosdaniel.londono@utp.edu.co>
2022-01-25 20:43:50 -0500

    Fix Redis

--- api/utils/redis.js
+++ api/utils/redis.js
@@ -1,15 +1,14 @@
 const redis = require("redis")
 
 let client
-if (process.env.NODE_ENV === 'development') {
-  client = redis.createClient()
-}
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   client = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD
   })
+} else {
+  client = redis.createClient()
 }
 
 client.on("error", function(error) {
