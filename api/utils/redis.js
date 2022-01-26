ad518ddf33694d15293fb977ee65033afcac2597
Carlos <carlosdaniel.londono@utp.edu.co>
2022-01-25 20:40:06 -0500

    Config Redis for production

--- api/utils/redis.js
+++ api/utils/redis.js
@@ -1,5 +1,16 @@
 const redis = require("redis")
-const client = redis.createClient()
+
+let client
+if (process.env.NODE_ENV === 'development') {
+  client = redis.createClient()
+}
+if (process.env.NODE_ENV === 'development') {
+  client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+  })
+}
 
 client.on("error", function(error) {
   console.error(error)
