ff4b61f28d88f42b2427e2a2497b0c4ff0789f89
Carlos <carlosdaniel.londono@utp.edu.co>
2022-04-11 15:01:55 -0500

    Twilio

---
+++ api/utils/twilio.js
@@ -0,0 +1,24 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+class Twilio {
+  static async sendCodeBySms(phone, code) {
+    return await client.messages
+      .create({
+        from: `${process.env.TWILIO_PHONE}`,
+        body: `Your code is ${code}`,
+        to: `+57${phone}`
+      })
+  }
+  static async sendCodeByWhatsApp(phone, code) {
+    return await client.messages
+      .create({
+        from: `whatsapp:${process.env.TWILIO_WHATSAPP}`,
+        body: `Your code is ${code}`,
+        to: `whatsapp:+57${phone}`
+      })
+  }
+}
+
+module.exports = Twilio
