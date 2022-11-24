ebf49bc2902bb147531262b2e6277e196a98ab67
Carlos <carlosdaniel.londono@utp.edu.co>
2022-11-24 12:29:33 -0500

    Setting up the standard

--- api/database/models/index.js
+++ api/database/models/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config.js')[env]
+const config = require(path.join(__dirname, '/../config.js'))[env]
 const db = {}
 
 let sequelize
