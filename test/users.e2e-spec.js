f3a86a6252645f39cc8a27d23f6a710f280da1b2
Carlos <carlosdaniel.londono@utp.edu.co>
2023-02-01 11:46:12 -0500

    Adding e2e tests

--- test/users.e2e-spec.js
+++ test/users.e2e-spec.js
@@ -8,37 +8,37 @@ describe('Users Endpoints', () => {
 
   test('POST /api/users/sign-up', async () => {
     const response = await request(app).post('/api/users/sign-up').send({
-      name: 'Samuel Cataño Londoño',
-      birthday: '2008-01-02',
+      name: 'Carlos Daniel Londoño',
+      birthday: '2001-08-10',
       gender: 'male',
-      phone: '3145406608',
-      email: 'samuelcataño@gmail.com',
-      password: 'samuel123'
+      phone: '3205821741',
+      email: 'carlosdaniel.londono@utp.edu.co',
+      password: '12345678'
     })
     expect(response.status).toBe(201)
     expect(response.body.message).toEqual('You have registered successfully')
     expect(response.body.data).toMatchObject({
-      name: 'Samuel Cataño Londoño',
-      birthday: '2008-01-02',
+      name: 'Carlos Daniel Londoño',
+      birthday: '2001-08-10',
       gender: 'male',
-      phone: '3145406608',
-      email: 'samuelcataño@gmail.com'
+      phone: '3205821741',
+      email: 'carlosdaniel.londono@utp.edu.co'
     })
   })
 
   test('POST /api/users/sign-in', async () => {
     const response = await request(app).post('/api/users/sign-in').send({
-      email: 'samuelcataño@gmail.com',
-      password: 'samuel123'
+      email: 'carlosdaniel.londono@utp.edu.co',
+      password: '12345678'
     })
     expect(response.status).toBe(200)
     expect(response.body.message).toEqual('You have authenticated successfully')
     expect(response.body.data.user).toMatchObject({
-      name: 'Samuel Cataño Londoño',
-      birthday: '2008-01-02',
+      name: 'Carlos Daniel Londoño',
+      birthday: '2001-08-10',
       gender: 'male',
-      phone: '3145406608',
-      email: 'samuelcataño@gmail.com'
+      phone: '3205821741',
+      email: 'carlosdaniel.londono@utp.edu.co'
     })
     expect(response.body.data.token).toBeDefined()
     token = response.body.data.token
@@ -50,17 +50,17 @@ describe('Users Endpoints', () => {
       .set('Authorization', `Bearer ${token}`)
     expect(response.body.message).toEqual('You have authenticated successfully')
     expect(response.body.data.user).toMatchObject({
-      name: 'Samuel Cataño Londoño',
-      birthday: '2008-01-02',
+      name: 'Carlos Daniel Londoño',
+      birthday: '2001-08-10',
       gender: 'male',
-      phone: '3145406608',
-      email: 'samuelcataño@gmail.com'
+      phone: '3205821741',
+      email: 'carlosdaniel.londono@utp.edu.co'
     })
     expect(response.body.data.token).toBeDefined()
   })
 
   afterAll(async () => {
-    await User.destroy({ where: { email: 'samuelcataño@gmail.com' } })
+    await User.destroy({ where: { email: 'carlosdaniel.londono@utp.edu.co' } })
     server.close()
     // Redis.quit()
   })
