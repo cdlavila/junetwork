4a6630a2f81a5143121d6015b9241971d10091ca
Carlos <carlosdaniel.londono@utp.edu.co>
2023-02-01 15:39:02 -0500

    Final version CI

--- src/database/config.js
+++ src/database/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    url: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    url: `postgres://${process.env.DATABASE_USER_DEV}:${process.env.DATABASE_PASSWORD_DEV}@${process.env.DATABASE_HOST_DEV}:${process.env.DATABASE_PORT_DEV}/${process.env.DATABASE_NAME_DEV}`,
     dialect: 'postgres',
     logging: false,
     define: {
@@ -11,7 +11,7 @@ module.exports = {
     }
   },
   test: {
-    url: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST_TEST}:${process.env.DATABASE_PORT_TEST}/${process.env.DATABASE_NAME}`,
+    url: `postgres://${process.env.DATABASE_USER_TEST}:${process.env.DATABASE_PASSWORD_TEST}@${process.env.DATABASE_HOST_TEST}:${process.env.DATABASE_PORT_TEST}/${process.env.DATABASE_NAME_TEST}`,
     dialect: 'postgres',
     logging: false,
     define: {
@@ -20,7 +20,7 @@ module.exports = {
     }
   },
   production: {
-    url: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    url: `postgres://${process.env.DATABASE_USER_PROD}:${process.env.DATABASE_PASSWORD_PROD}@${process.env.DATABASE_HOST_PROD}:${process.env.DATABASE_PORT_PROD}/${process.env.DATABASE_NAME_PROD}`,
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
